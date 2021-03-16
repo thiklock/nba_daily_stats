0289f7f18e10e60201153ef8fe1cf412c6bfb2bd
klock <thiago.klock@indicium.tech>
2021-03-16 08:04:48 -0300

    URL as enviromental variable

--- loader.py
+++ loader.py
@@ -1,16 +1,20 @@
 """
 This file shall load df inputs to postgres database all functions of the project
 """
-
+import os
 from library import SqlConnection, Extraction
 import pandas as pd
+from decouple import config
 from sqlalchemy import create_engine
+
+
+URL = config('db_url')
 
 
 
 df = pd.read_csv("nba_stats_21.csv")
 
-connection = connection
+connection = URL
 
 df_replace = SqlConnection.to_sql_replace(df, connection, "nba_stats_21_snapshot")
 
